e42ddd98b8c90acbec039056e0075c9df623f2f7
GuiDev45 <bortolettodev@gmail.com>
2022-08-02 20:15:12 -0300

    Projeto criado

---
+++ LancheMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LancheMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
