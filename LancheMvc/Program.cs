b71a1e951deb4602609f19d4e9a2dc438b093567
GuiDev45 <bortolettodev@gmail.com>
2022-08-03 17:56:00 -0300

    Anotações Seção 02 - 13

--- LancheMvc/Program.cs
+++ LancheMvc/Program.cs
@@ -1,27 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace LancheMvc;
+public class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //O método Main configura o ASP.NET Core e inicia a aplicação.
+    public static void Main(string[] args)
+    {
+        //Método CreateHostBuilder, retorna um objeto CreateHostBuilder que implementa uma interface IHostBuilder.
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args) //Host, é usada para criar uma classe de instância IHostBuilder,
+                                        //Essa configurações são pré definidas,
+                                        //CreateDefaultBuilder vai configurar um Host para as configurações web padrão.
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>(); //Método UseStartup, chamando a classe Startup.
+            });
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
---
+++ LancheMvc/Startup.cs
@@ -0,0 +1,48 @@
+﻿namespace LancheMvc;
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    //Método ConfigureServices, configura os serviços do aplicativo
+    public void ConfigureServices(IServiceCollection services)
+    {
+        //Por hora apenas os /controladores de Views, AddControllersWithViews/ está sendo usado
+        services.AddControllersWithViews();
+    }
+
+    //Método Configure, configura o pipeline de processamento do aplicativo
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        //Verificando SE o ambiente é um ambiente de desenvolvimento
+        if (env.IsDevelopment())
+        {
+            //E SE estiver em um ambiente de desenvolvimento vai usar essa página de exceção que mostra erros mais detalhados.
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error"); //Página de exceção mais específica que por hora não vai ser configurada.
+            app.UseHsts();
+        }
+        //UseHttpsRedirection habilita o pipeline para o redirecionamento do Https.
+        app.UseHttpsRedirection();
+        //UseStaticFiles, para usar os arquivos statics que estão na pasta wwwroot, pasta css, js, etc.
+        app.UseStaticFiles();
+        //UseRouting, Pipeline de roteamento que permite definir o mapeamento/são os UseEndpoints.
+        app.UseRouting();
+        //UseAuthorization, habilita a autorização na aplicação.
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(//Definição do mapeamento padrão
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
