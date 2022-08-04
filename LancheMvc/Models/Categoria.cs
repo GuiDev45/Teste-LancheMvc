3086550c8d7a614cb1f336162e8d2bd6892022ac
GuiDev45 <bortolettodev@gmail.com>
2022-08-04 17:18:32 -0300

    Modelo de dominio e anotações

---
+++ LancheMvc/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿namespace LancheMvc.Models
+{
+    public class Categoria
+    {
+        //Propriedade com Id ou NomeEntidadeId vai gerar uma chave primária na tabela.
+        //Propriedades da classe vão gerar colunas com o mesmo nome na tabela.
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+
+        //Relacionamento (1 - N) entre a entidade Categoria e a entidade Lanche.
+        public List<Lanche> Lanches { get; set; }
+    }
+}
---
+++ LancheMvc/Models/Lanche.cs
@@ -0,0 +1,23 @@
+﻿namespace LancheMvc.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        //Relacionamento entre as entidades/propriedade de navegação,
+        //relacionamento entre Lanche e Categoria, CategoriaId vai ser a chave estrangeira.
+        //Esses dois atributos não vão ser criados na tabela do banco de dados, 
+        //vão ser usados para definir o relacionamento entre Lanche e Categoria
+        public int CategoriaId { get; set; }
+        //Propriedade de navegação Categora Categoria.
+        public virtual Categoria Categoria { get; set; }
+    }
+}
